b84509ddf0457f5a792f423eee93be18e9da9949
Alirio <romeroaj.20@gmail.com>
2026-08-23 12:23:18 +0200

    Initials arragements

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+let initialCards = [
+  { name: "Card 1", link: "https://example.com/card1" },
+  { name: "Card 2", link: "https://example.com/card2" },
+  { name: "Card 3", link: "https://example.com/card3" },
+  { name: "Card 4", link: "https://example.com/card4" },
+  { name: "Card 5", link: "https://example.com/card5" },
+  { name: "Card 6", link: "https://example.com/card6" },
+];
+
+console.log(initialCards);
