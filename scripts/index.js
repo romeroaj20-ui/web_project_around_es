c580c53e9e4fd740b179b25db11d76c02259ca97
Alirio <romeroaj.20@gmail.com>
2026-08-26 02:55:43 +0200

    Thirds arregement

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,45 @@ let initialCards = [
   },
 ];
 
+const editProfileBtn = document.querySelector(".profile__edit-button");
+const editProfilePopup = document.querySelector("#edit-popup");
+const closeEditProfileBtn = editProfilePopup.querySelector(".popup__close");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const nameInput = formElement.querySelector(".popup__input_type_name");
+const descriptionInput = formElement.querySelector(
+  ".popup__input_type_description",
+);
+const formElement = document.querySelector(".popup__form");
+
+function openModal() {
+  editProfilePopup.classList.add("popup_is-opened");
+}
+
+function closeModal() {
+  editProfilePopup.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal();
+}
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closeModal();
+}
+
 initialCards.forEach((card) => {
   console.log(card.name);
 });
+editProfileBtn.addEventListener("click", handleOpenEditModal);
+closeEditProfileBtn.addEventListener("click", closeModal);
+formElement.addEventListener("submit", handleProfileFormSubmit);
